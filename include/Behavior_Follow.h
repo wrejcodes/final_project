42aafa27f0653ea1b299f276aea99e552a1e70c6
timthom64 <timthom64@gmail.com>
2017-11-28 18:15:11 -0600

    Beer Can on Roomba and Behavior Follow Almost Ready for test

--- include/Behavior_Follow.h
+++ include/Behavior_Follow.h
@@ -5,31 +5,62 @@
 #include "final_project/behavior.h"
 #include "sensor_msgs/LaserScan.h"
 
-#define BH_DRIVE_RATE 70
-
+#define BH_FOLLOW_RATE 70
 
 class Behavior_Follow{
 public:
-	static constexpr double DRIVE_SPEED = .25f;
-	static constexpr double DRIVE_TURN = .15f;
-	static constexpr double MID_LASER_THRESHOLD = .9f;
-	static constexpr double DELTA_THRESHOLD = .15f;
+	
+	//static constexpr double DRIVE_TURN = .15f;
+	//static constexpr double DELTA_THRESHOLD = .15f;
+	
+	//Follow Math Constants
+	static constexpr double DESIRED_FOLLOW_DISTANCE = 1.00f;
+	static constexpr double FWD_VEL = 0.25f;		
+	static constexpr double TRN_VEL = 0.25f;
+	static constexpr double TRN_THRESHOLD = 0.15f;
+
+	//The boundaries for a front pointing ray.
+	static constexpr int FOLLOW_RANGE_SIZE = 15; 
+
+	//PID Constants
+	double F_P_GAIN = 1;
+	double F_I_GAIN = 1;
+	double F_D_GAIN = 1;
+	double T_P_GAIN = 1;
+	double T_I_GAIN = 1;
+	double T_D_GAIN = 1;
+
+	//Don't start detecting until
+	static constexpr double TRIGGER_FOLLOW_DISTANCE = 4.00f;
 
 	Behavior_Follow();
 
-	void center_scan_callback(const sensor_msgs::LaserScan::ConstPtr& msg);
+	void hokuyo_callback(const sensor_msgs::LaserScan::ConstPtr& msg);
 	void process_behavior();
 	
 private:
+	//Handle, Publisher, Subscriber
 	ros::NodeHandle nh;
-
 	ros::Publisher pub_arbiter;
-
-	ros::Subscriber left_side_laser;
-	ros::Subscriber right_side_laser;
-
-	double center_min;
-	double center_max;
+	ros::Subscriber hokuyo_scan;
+	
+	//Average distances
+	double hokuyo_right_avg;
+	double hokuyo_center_avg;
+	double hokuyo_left_avg;
+	
+	//FORWARD PID CONTROL VARIABLES
+	double f_pid;
+	double f_sum;
+	double f_last_error;
+	double f_error;
+	
+	//TURN PID CONTROL VARIABLES
+	double t_pid;
+	double t_sum;
+	double t_last_error;
+	double t_error;
+	
 };
 
 #endif
--- src/Behavior_Follow.cpp
+++ src/Behavior_Follow.cpp
@@ -1,64 +1,123 @@
 #include "Behavior_Follow.h"
 
 Behavior_Follow::Behavior_Follow(){
-	// set up initial stuff
-	center_min = 1.5f;
-	center_max = 3.0f
+	//Set up initial stuff
+	//Average distances
+	hokuyo_right_avg = 0;
+	hokuyo_center_avg = 0;
+	hokuyo_left_avg = 0;
+	
+	//FORWARD PID CONTROL VARIABLES
+	f_pid = 0;
+	f_sum = 0;
+	f_last_error = 0;
+	f_error = 0;
+	
+	//TURN PID CONTROL VARIABLES
+	t_pid = 0;
+	t_sum = 0;
+	t_last_error = 0;
+	t_error = 0;
+
 	// publisher
 	pub_arbiter = nh.advertise<final_project::behavior>("behavior/follow", 1);
-	// subscribe
-	center_hokuyo_laser = nh.subscribe("left_side_scan", 1, &Behavior_Drive::center_scan_callback, this);
 	
+	// subscribe
+	hokuyo_laser = nh.subscribe("hokuyo_scan", 1, &Behavior_Follow::hokuyo_center_callback, this);
 }
 
-void Behavior_Drive::center_scan_callback(const sensor_msgs::LaserScan::ConstPtr& msg){
-	left_back = msg->ranges[0];
-	left_mid = msg->ranges[1];
-	left_front = msg->ranges[2];
+//hokuyo_callback
+void Behavior_Drive::hokuyo_callback(const sensor_msgs::LaserScan::ConstPtr& msg){
+	int start = 180 + FOLLOW_RANGE_SIZE/2;
+	int end = 180 - FOLLOW_RANGE_SIZE/2;
+	int offset = FOLLOW_RANGE_SIZE/3;
+
+	double hokuyo_left_sum = 0;
+	double hokuyo_center_sum = 0;
+	double hokuyo_right_sum = 0;
+		
+	for(int i = start; i < end; ++i){
+		//right side
+		if(i >= start && i < start + offset){
+			hokuyo_right_sum += msg->ranges[i];
+		}
+		//center side
+		else if(i >= start + offset && i < end - offset){
+			hokuyo_center_sum += msg->ranges[i];
+		}
+		//left side
+		else{
+			hokuyo_left_sum += msg->ranges[i];
+		}
+	}
+	
+	//averages
+	hokuyo_right_avg = hokuyo_right_sum / FOLLOW_RANGE_SIZE;
+	hokuyo_center_avg = hokuyo_center_sum / FOLLOW_RANGE_SIZE;
+	hokuyo_left_avg = hokuyo_left_sum / FOLLOW_RANGE_SIZE;
+
+	//forward pid
+	f_error = DESIRED_FOLLOW_DISTANCE - hokuyo_center_avg;
+	f_sum += f_error; 
+	f_pid = (F_P_GAIN * f_error) + (F_I_GAIN * ((1/loop_rate)*f_sum)) + (F_D_GAIN * ((f_error - f_last_error)/(1/loop_rate)))
+	f_last_error = f_error;
+
+	//turn pid	
+
+
+	if(hokuyo_center_avg > DESIRED_FOLLOW_DISTANCE){
+		f_error = 0;
+		f_last_error = 0;
+		f_sum = 0;
+		
+		t_error = 0;
+		t_last_error = 0;
+		t_sum = 0;
+	}
 }
 
-void Behavior_Drive::process_behavior(){
+void Behavior_Follow::process_behavior(){
 	final_project::behavior msg_move;
 	msg_move.vel_fw = 0;
 	msg_move.vel_turn = 0;
 	msg_move.active = false;
 	
-	if(right_mid <= MID_LASER_THRESHOLD){
-		// follow right
-		if(right_front > right_back + DELTA_THRESHOLD){
-			msg_move.vel_turn = -DRIVE_TURN;
-		} else if(right_back > right_front + DELTA_THRESHOLD){
-			msg_move.vel_turn = DRIVE_TURN;
-		}
-		msg_move.vel_fw = DRIVE_SPEED;
+	//Trigger to begin follow forward
+	if(TRIGGER_FOLLOW_DISTANCE > hokuyo_center_avg_range){
+		msg_move.vel_fw = f_pid;
+		msg_move.vel_turn = 0;
 		msg_move.active = true;
-	} else if(left_mid <= MID_LASER_THRESHOLD){
-		// follow left
-		if(left_front > left_back + DELTA_THRESHOLD){
-			msg_move.vel_turn = DRIVE_TURN;
-		} else if(left_back > left_front + DELTA_THRESHOLD){
-			msg_move.vel_turn = -DRIVE_TURN;
-		}
-		msg_move.vel_fw = DRIVE_SPEED;
-		msg_move.active = true;
-	}
-	
-	pub_arbiter.publish(msg_move);
 
+		//Trigger to turn left
+		if(hokuyo_right_avg > hokuyo_left_avg){
+			msg_move.vel_fw = f_pid;
+			msg_move.vel_turn = t_pid;
+			msg_move.active = true;
+		}
+
+		//Trigger to turn right
+		if(hokuyo_left_avg > hokuyo_right_avg){
+			msg_move_vel_fw = f_pid;
+			msg_move.vel_turn = -t_pid;
+			msg_move.active = true;
+		}
+	}	
+
+	pub_arbiter.publish(msg_move);
 }
 
 int main(int argc, char **argv){
-	ros::init(argc, argv, "fp_bh_drive");
+	ros::init(argc, argv, "fp_bh_follow");
 
-	ROS_INFO("Drive behavior node started...");
+	ROS_INFO("Follow behavior node started...");
 
-	Behavior_Drive driver;
-	ros::Rate loop_rate(BH_DRIVE_RATE);
+	Behavior_Follow following;
+	ros::Rate loop_rate(BH_FOLLOW_RATE);
 
 	while(ros::ok()){
-		driver.process_behavior();
+		following.process_behavior();
 		ros::spinOnce();
-		if(BH_DRIVE_RATE != 0) {
+		if(BH_FOLLOW_RATE != 0) {
 			loop_rate.sleep();
 		}
 	}
